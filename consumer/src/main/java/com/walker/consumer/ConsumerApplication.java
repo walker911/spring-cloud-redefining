e2a209cda68a6e734fb43d9554ae8e366dd4d0b0
walker <qinmu911@163.com>
2018-12-28 15:29:32 +0800

    refactor(annotation): use enable discovery client

--- consumer/src/main/java/com/walker/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/walker/consumer/ConsumerApplication.java
@@ -2,15 +2,15 @@ package com.walker.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author walker
  * @date 2018/12/26
  */
-@EnableEurekaClient
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerApplication {
     public static void main(String[] args) {
--- provider/src/main/java/com/walker/provider/ProviderApplication.java
+++ provider/src/main/java/com/walker/provider/ProviderApplication.java
@@ -2,13 +2,13 @@ package com.walker.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author walker
  * @date 2018/12/26
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderApplication {
     public static void main(String[] args) {
